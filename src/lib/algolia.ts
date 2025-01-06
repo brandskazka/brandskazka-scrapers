4ad5af53db0d9182dbafc66b46160143e912b847
Safe <safe@kali.safe>
2025-01-06 10:58:23 +0300

    updated the algolia

--- src/lib/algolia.ts
+++ src/lib/algolia.ts
@@ -1,6 +1,6 @@
 import algoliasearch from "algoliasearch";
 
-const client = algoliasearch("P80WOP9K0O", "7abb32d0f1e554cbeb81e6c8a1c3529d");
+const client = algoliasearch("N8MQF53XOS", "dc3911b9cd576f3e6b58360095b1a21a");
 export const brandskazka_index = client.initIndex("brandskazka_products");
 export const mode_wave_index = client.initIndex("mode_wave_products");
 // export const index = client.initIndex("products_raw");
